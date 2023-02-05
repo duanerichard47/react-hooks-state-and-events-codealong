9bd5a4db6372508f012c0b366df074bf07e6cd5a
Duane <duanerichard47@hotmail.com>
2023-02-05 13:03:33 -0500

    completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,36 @@
 import React from "react";
 import {useState} from "react";
 
+
+
+// my incorrect attempt
+// function Toggle () {
+
+//   function handleClick(){
+    
+//     const [isOn, setIsOn] = useState(true)  // should be outside because setting global variable
+//     setIsOn(!isOn)
+//   }
+
+// color ? backgroundColor = 'white' : backgroundColor = 'red'
+
+
+// return(
+// //  <button style = {{color : color}}>ON</button>
+// <button onClick={handleClick}>{isOn ? 'ON' : 'OFF' }</button>
+// )
+// }
+// export default Toggle
+
+
+
 function Toggle() {
   const [isOn, setIsOn ] = useState(false);
 
   function handleClick ()
   {
-    setIsOn((isOn) => !isOn );
+    // setIsOn((isOn) => !isOn );
+    setIsOn(!isOn)    //either way works for now
 
     
   }
